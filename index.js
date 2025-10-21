042e1c07b89eda1d3c578cf390fbddaed7b7fa50
fikrimhammd <fikri.kdi60@gmail.com>
2025-10-21 15:10:40 +0700

    Koneksi ke database

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const express = require("express")
-let mysql =
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const PORT = 3000;
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Kendari2023",
+  database: "mahasiswa",
+  port: 3309,
+});
 
