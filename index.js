7233cd6c2b6c1cb420a135df8cec9cc94896607b
fikrimhammd <fikri.kdi60@gmail.com>
2025-10-21 15:14:05 +0700

    PUT update data mahasiswa

--- index.js
+++ index.js
@@ -63,3 +63,22 @@ app.post("/api/mahasiswa", (req, res) => {
   );
 });
 
+// ✅ PUT update data mahasiswa
+app.put("/api/mahasiswa/:id", (req, res) => {
+  const userId = req.params.id;
+  const { nama, alamat, agama } = req.body;
+
+  db.query(
+    "UPDATE biodata SET nama = ?, alamat = ?, agama = ? WHERE id = ?",
+    [nama, alamat, agama, userId],
+    (err, results) => {
+      if (err) {
+        console.error("Database Error:", err);
+        return res.status(500).json({ message: "Database Error" });
+      }
+      res.json({ message: "Mahasiswa berhasil diperbarui" });
+    }
+  );
+});
+
+
