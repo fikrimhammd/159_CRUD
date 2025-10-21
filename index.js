990d54a904128e5774953287a39bd51c9d502f58
fikrimhammd <fikri.kdi60@gmail.com>
2025-10-21 15:11:33 +0700

    Endpoint root

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const mysql = require("mysql2");
 const app = express();
 const PORT = 3000;
 
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Koneksi ke database
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Kendari2023",
   database: "mahasiswa",
   port: 3309,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    return;
+  }
+  console.log("Koneksi ke database berhasil!");
+});
+
+// Endpoint root
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
